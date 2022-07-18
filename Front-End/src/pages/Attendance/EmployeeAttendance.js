9e6d48a85bf9990f4e1784652f00d5d59a10878a
tarun <tarun.m@pyther.com>
2022-07-19 01:32:38 +0530

    [Attendance Working]

--- Front-End/src/pages/Attendance/EmployeeAttendance.js
+++ Front-End/src/pages/Attendance/EmployeeAttendance.js
@@ -1,191 +1,94 @@
 // src/components/filter.
 import React, { useState, useMemo, useEffect } from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 //import components
-import Breadcrumbs from '../../components/Common/Breadcrumb';
-import TableContainer from '../../components/Common/TableContainer';
+import Breadcrumbs from "../../components/Common/Breadcrumb";
+import TableContainer from "../../components/Common/TableContainer";
 import { apiservice } from "service/apiservice";
-import { USERS } from 'service/url_helper'
+import { USERS } from "service/url_helper";
+import { get, reject } from "lodash";
 
-function Attendance() {
-    const [users, setUser] = useState();
+const Attendance = () => {
+  const [users, setUser] = useState();
+  
+  useEffect(() => {
+    console.log("Use Effect")
+    console.log(localStorage.getItem('token'));
+    apiservice.callServiceGet(USERS)
+      .then((user) => {
+        console.log(user.data.result);
+        setUser(user.data.result)
+      })
+      .catch((e) => { console.log(e) })
 
-    let data;
-    useEffect(() => {
-        
-        console.log("In Use Effect",localStorage.getItem('token'));
-        apiservice.callServiceGet(USERS)
-          .then((user) => {
+  }, [])
 
-            console.log("Users are",user.data.result);
-            setUser(user.data.result)
-          })
-          .catch((e) => { console.log(e) })
-    
-      });
-    
-    let user = localStorage.getItem('user');
-    console.log("current user is",user);
-    console.log("Users is ",users);
-
-    // const [users, setUsers] = useState();
-    const columns = useMemo(
-        () => [
-            {
-                Header: "#",
-                Cell: () => {
-                    return <input type="checkbox" />;
-                },
-            },
-            {
-                Header: 'Name',
-                accessor: 'name',
-            },
-            {
-                Header: 'Designation',
-                accessor: 'designation'
-            },
-            {
-                Header:'email address',
-                accessor:'email'
-            }
-        ],
-        []
-    );
-
-     data = [
-        {
-            "name": "horn-od926",
-            "designation": "admin",
-            "email":"a@b.com"
+  let data;
+  const columns = useMemo(
+    () => [
+      {
+        Header: "#",
+        Cell: () => {
+          return <input type="checkbox" />;
         },
-        {
-            "name": "heart-nff6w",
-            "designation": 'employee',
-            "email":'b@a.com'
+      },
+      {
+        Header: "Name",
+        accessor: "name",
+      },
+      {
+        Header: "Designation",
+        accessor: "designation",
+      },
+      {
+        Header: "email address",
+        accessor: "email",
+      },
+    ],
+    []
+  );
 
-        },
-        // {
-        //     "firstName": "minute-yri12",
-        //     "lastName": "fairies-iutct",
-        //     "age": 7,
-        //     "visits": 77,
-        //     "progress": 39,
-        //     "status": "single"
-        // },
-        // {
-        //     "firstName": "degree-jx4h0",
-        //     "lastName": "man-u2y40",
-        //     "age": 27,
-        //     "visits": 54,
-        //     "progress": 92,
-        //     "status": "relationship"
-        // },
-        // {
-        //     "firstName": "horn-od926",
-        //     "lastName": "selection-gsykp",
-        //     "age": 22,
-        //     "visits": 20,
-        //     "progress": 39,
-        //     "status": "single"
-        // },
-        // {
-        //     "firstName": "heart-nff6w",
-        //     "lastName": "information-nyp92",
-        //     "age": 16,
-        //     "visits": 98,
-        //     "progress": 40,
-        //     "status": "complicated"
-        // },
-        // {
-        //     "firstName": "minute-yri12",
-        //     "lastName": "fairies-iutct",
-        //     "age": 7,
-        //     "visits": 77,
-        //     "progress": 39,
-        //     "status": "single"
-        // },
-        // {
-        //     "firstName": "degree-jx4h0",
-        //     "lastName": "man-u2y40",
-        //     "age": 27,
-        //     "visits": 54,
-        //     "progress": 92,
-        //     "status": "relationship"
-        // },
-        // {
-        //     "firstName": "horn-od926",
-        //     "lastName": "selection-gsykp",
-        //     "age": 22,
-        //     "visits": 20,
-        //     "progress": 39,
-        //     "status": "single"
-        // },
-        // {
-        //     "firstName": "heart-nff6w",
-        //     "lastName": "information-nyp92",
-        //     "age": 16,
-        //     "visits": 98,
-        //     "progress": 40,
-        //     "status": "complicated"
-        // },
-        // {
-        //     "firstName": "minute-yri12",
-        //     "lastName": "fairies-iutct",
-        //     "age": 7,
-        //     "visits": 77,
-        //     "progress": 39,
-        //     "status": "single"
-        // },
-        // {
-        //     "firstName": "degree-jx4h0",
-        //     "lastName": "man-u2y40",
-        //     "age": 27,
-        //     "visits": 54,
-        //     "progress": 92,
-        //     "status": "relationship"
-        // }
-    ];
-    // data = users;
+  data = [
+    
+  ];
+  // data = users;
 
-
-    //meta title
-    document.title = "Attendance | HRMS";
-    // useEffect(() => {
-    //     console.log(localStorage.getItem('token'));
-    //     apiservice.callServiceGet(USERS)
-    //         .then((user) => {
-    //             console.log(user.data.result);
-    //             setUsers(user.data.result)
-    //         })
-    //         .catch((e) => { console.log(e) })
-
-    // }, [])
-
-
-    return (
+  //meta title
+  document.title = "Attendance | HRMS";
+        return (
         <div className="page-content">
-            <div className="container-fluid">
-                <Breadcrumbs title="Tables" breadcrumbItem="Data Tables" />
-                {/* <Table columns={columns} data={data} /> */}
+          <div className="container-fluid">
+            <Breadcrumbs title="Tables" breadcrumbItem="Data Tables" />
+            {
+              users?<TableContainer
+              columns={columns}
+              data={users}
+              isGlobalFilter={true}
+              isAddOptions={true}
+              customPageSize={10}
+              className="custom-header-css"
+            />:
+            <TableContainer
+              columns={columns}
+              data={data}
+              isGlobalFilter={true}
+              isAddOptions={true}
+              customPageSize={10}
+              className="custom-header-css"
+            />
+            }
             
-                <TableContainer
-                    columns={columns}
-                    data={users}
-                    isGlobalFilter={true}
-                    isAddOptions={true}
-                    customPageSize={10}
-                    className="custom-header-css"
-                />
-            </div>
+            
+
+            
+          </div>
         </div>
-    );
-}
-Attendance.propTypes = {
-    preGlobalFilteredRows: PropTypes.any,
-
+        )
+   
 };
-
+Attendance.propTypes = {
+  preGlobalFilteredRows: PropTypes.any,
+};
 
 export default Attendance;
